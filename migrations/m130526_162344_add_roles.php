295951bb50c590f8b6b318b6c867a6d049e57a9a
Gary Burge <garyburge@garyburge.com>
2013-07-13 06:49:33 -0400

    Fix foreign key constraints for AuthAssignment

--- migrations/m130526_162344_add_roles.php
+++ migrations/m130526_162344_add_roles.php
@@ -59,8 +59,8 @@ class m130526_162344_add_roles extends CDbMigration
                 ), $this->MySqlOptions);
                 $this->createIndex('itemname', Yii::app()->getModule('role')->tableAuthAssignment, 'itemname', false);
                 $this->createIndex('userid', Yii::app()->getModule('role')->tableAuthAssignment, 'userid', false);
-                $this->addForeignKey('itemname', Yii::app()->getModule('role')->tableAuthAssignment, 'itemname', Yii::app()->getModule('role')->tableAuthItem, 'name', 'CASCADE', 'RESTRICT');
-                $this->addForeignKey('user', Yii::app()->getModule('role')->tableAuthAssignment, 'userid', Yii::app()->getModule('user')->tableUsers, 'id', 'CASCADE', 'RESTRICT');
+                $this->addForeignKey('itemname', Yii::app()->getModule('role')->tableAuthAssignment, 'itemname', Yii::app()->getModule('role')->tableAuthItem, 'name', 'CASCADE', 'CASCADE');
+                $this->addForeignKey('user', Yii::app()->getModule('role')->tableAuthAssignment, 'userid', Yii::app()->getModule('user')->tableUsers, 'id', 'CASCADE', 'CASCADE');
                 break;
 
             case "sqlite":
